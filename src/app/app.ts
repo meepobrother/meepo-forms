724471affb6fe4651035fe3ab26f948afa3a3f91
15138730370 <1037483576@qq.com>
2017-12-24 11:42:15 +0800

    save to back

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,14 +5,18 @@ import { FormRootComponent } from './form-root/form-root';
 import { FormRowComponent } from './form-row/form-row';
 import { FormInputComponent } from './form-input/form-input';
 import { FormTextareaComponent } from './form-textarea/form-textarea';
+import { FormImageComponent } from './form-image/form-image';
+
 
 export const formsComponents = [
     FormAddressComponent,
     FormRootComponent,
     FormRowComponent,
     FormInputComponent,
-    FormTextareaComponent
+    FormTextareaComponent,
+    FormImageComponent
 ];
+
 import { UuidModule } from 'meepo-uuid';
 import { MeepoBmapModule } from 'meepo-bmap';
 import { AutoSizeModule } from 'meepo-autosize';
---
+++ src/app/form-image/form-image.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'form-image',
+    templateUrl: './form-image.html',
+    styleUrls: [
+        './form-image.scss'
+    ]
+})
+export class FormImageComponent implements OnInit {
+    currentLength: number = 0;
+    @Input() max: number = 6;
+    @Input() model: any[] = [];
+    @Output() modelChange: EventEmitter<any> = new EventEmitter();
+    constructor() { }
+
+    ngOnInit() { }
+}

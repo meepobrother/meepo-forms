8fe8d312ef537c7f8478cf4d1b0da130d9d02d49
15138730370 <1037483576@qq.com>
2017-12-28 10:20:36 +0800

    save to back

--- src/app/form-select/form-select.ts
+++ src/app/form-select/form-select.ts
@@ -7,17 +7,33 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 
 export class FormSelectComponent implements OnInit {
-    @Input() items: any[] = [];
-
+    _items: any[] = [];
+    @Input()
+    set items(val: any[]) {
+        this._items = val;
+        this.setDefault();
+    }
+    get items() {
+        return this._items;
+    }
     @Output() modelChange: EventEmitter<any> = new EventEmitter();
+    active: any;
     constructor() { }
 
     ngOnInit() { }
 
+    setDefault() {
+        this._items.map(res => {
+            if (res.active) {
+                this.active = res;
+            }
+        });
+    }
+
     _change(e: any) {
         let value = e.target.value;
         this.items.map(res => {
-            if (res.value === value) { 
+            if (res.value === value) {
                 this.modelChange.next(res);
             }
         });

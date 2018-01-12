8c1f0c0ff8b27699713629e1a3839036c0c2f345
15138730370 <1037483576@qq.com>
2018-01-12 09:50:21 +0800

    save to back

--- src/app/form-tags/form-tags.ts
+++ src/app/form-tags/form-tags.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, forwardRef, Input, Output, EventEmitter, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
+export const EXE_COUNTER_VALUE_ACCESSOR: any = {
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => FormTagsComponent),
+    multi: true
+};
 @Component({
     selector: 'form-tags',
     templateUrl: './form-tags.html',
     styleUrls: [
         './form-tags.scss'
     ],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        EXE_COUNTER_VALUE_ACCESSOR
+    ]
 })
-export class FormTagsComponent implements OnInit {
+export class FormTagsComponent implements ControlValueAccessor {
     @Input() items: any[] = [];
-    @Output() onSelect: EventEmitter<any> = new EventEmitter()
+    _model: any;
+    set model(val) {
+        this._model = val;
+        this.propagateChange(this._model);
+    }
+    get model() {
+        return this._model;
+    }
     constructor(
         public cd: ChangeDetectorRef
     ) { }
@@ -22,8 +38,22 @@ export class FormTagsComponent implements OnInit {
             res.active = false
         })
         item.active = true;
-        console.log(item);
-        this.onSelect.emit(item)
+        this.model = item;
         this.cd.detectChanges();
     }
+
+    propagateChange = (_: any) => { };
+
+    writeValue(value: any) {
+        if (value) {
+            this.model = value;
+        }
+    }
+
+    registerOnChange(fn: any) {
+        this.propagateChange = fn;
+    }
+
+    registerOnTouched(fn: any) { }
+
 }
